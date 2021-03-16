fb4290e59e7579e7bec87b171521362f1d4daf80
Eduardo Garcia <eduardo.garcia16@stjohns.edu>
2021-03-16 15:50:28 -0500

    Added a couple methods in respective repositories & corrected annotations

--- TaskAssess/src/main/java/dev/rev/repos/CategoryRepo.java
+++ TaskAssess/src/main/java/dev/rev/repos/CategoryRepo.java
@@ -1,9 +1,15 @@
 package dev.rev.repos;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.rev.beans.Category;
 
+@Repository
 public interface CategoryRepo extends CrudRepository<Category, Integer> {
-
+	
+	public Category findByCategory(String category);
+	public List<Category> findByColor(String color);
 }
--- TaskAssess/src/main/java/dev/rev/repos/UserRepo.java
+++ TaskAssess/src/main/java/dev/rev/repos/UserRepo.java
@@ -1,9 +1,13 @@
 package dev.rev.repos;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.rev.beans.User;
 
+@Repository
 public interface UserRepo extends CrudRepository<User, Integer>{
-
+	
+	public User findByEmail(String email);
+	public User findByEmailAndPassword(String email, String password);
 }
