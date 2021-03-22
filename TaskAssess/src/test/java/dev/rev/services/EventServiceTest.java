e75acfff900d1142f3f2c591ead7e572a514633f
Syd Rigel <prest261@umn.edu>
2021-03-22 12:32:59 -0600

    user services testing is completed, but still need to do service testing for categories and events

--- TaskAssess/src/test/java/dev/rev/services/EventServiceTest.java
+++ TaskAssess/src/test/java/dev/rev/services/EventServiceTest.java
@@ -1,10 +1,16 @@
 package dev.rev.services;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import dev.rev.beans.Category;
+import dev.rev.beans.Event;
+import dev.rev.beans.User;
 import dev.rev.repos.EventRepo;
 
 @SpringBootTest(classes = dev.rev.demo.TaskAssessApplication.class)
@@ -18,6 +24,53 @@ public class EventServiceTest {
 	
 	@Test
 	void testMethod() {
+		
+	}
+	
+	@Test
+	void getEvent() {
+		
+	}
+	
+	@Test
+	void getAllEvents() {
+		
+	}
+
+	@Test
+	void getEventsByUser() {
+		
+	}
+	
+	@Test
+	void addEvent() {
+//		Category testCat = new Category();
+//		User user = new User();
+//		Timestamp startTime = Timestamp.valueOf("2021-03-23 9:00:00.0");
+//		Timestamp endTime = Timestamp.valueOf("2021-03-23 10:00:00.0");
+//		Event e = new Event();
+//		Event eventTest = new Event();
+//		
+//		
+//		
+//		
+//		eventTest.setId(1);
+//		eventTest.setTitle("title");
+//		eventTest.setContent("content");
+//		eventTest.setStart(startTime);
+//		eventTest.setEnd(endTime);
+//		eventTest.setCategory(testCat);
+//		eventTest.setUser(user);
+//	
+	}
+	
+	@Test
+	void updateEvent() {
+		
+	}
+	
+	@Test
+	void deleteEvent() {
 		
 	}
 
--- TaskAssess/src/test/java/dev/rev/services/UserServiceTest.java
+++ TaskAssess/src/test/java/dev/rev/services/UserServiceTest.java
@@ -1,10 +1,22 @@
 package dev.rev.services;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import dev.rev.beans.User;
 import dev.rev.repos.UserRepo;
 
 @SpringBootTest(classes = dev.rev.demo.TaskAssessApplication.class)
@@ -20,5 +32,128 @@ public class UserServiceTest {
 	void testMethod() {
 		
 	}
-
+	
+	@Test
+	void addUser() {
+		
+		User user = new User();
+		User userTest = new User();
+		userTest.setId(1);
+		userTest.setEmail("test@gmail.com");
+		userTest.setPassword("password1");
+		userTest.setFirstName("Jane");
+		userTest.setLastName("Doe");
+		
+		Mockito.when(ur.save(userTest)).thenReturn(userTest);
+		
+		user= us.addUser(userTest);
+		Assertions.assertEquals(1, userTest.getId());
+	}
+	
+	@Test
+	void updateUser() {
+		
+		User userTest = new User();
+		userTest.setId(1);
+		userTest.setEmail("test@gmail.com");
+		userTest.setPassword("password1");
+		userTest.setFirstName("Jane");
+		userTest.setLastName("Doe");
+		
+		Mockito.when(ur.save(userTest)).thenReturn(userTest);
+		
+		Assertions.assertEquals("Jane", userTest.getFirstName());
+		userTest.setFirstName("Jill");
+		userTest = us.updateUser(userTest);
+		Assertions.assertEquals("Jill", userTest.getFirstName());
+		
+	}
+	
+	@Test
+	void deleteUser() {
+		
+		int id = 1;
+		User userTest = new User();
+		userTest.setId(id);
+		userTest.setEmail("test@gmail.com");
+		userTest.setPassword("password1");
+		userTest.setFirstName("Jane");
+		userTest.setLastName("Doe");
+		
+		Mockito.when(ur.findById(id)).thenReturn(Optional.of(userTest));
+		Mockito.when(ur.findById(0)).thenThrow(IllegalArgumentException.class);
+		
+		Assertions.assertEquals(true, us.deleteUser(id));
+		verify(ur,times(1)).delete(userTest);
+		
+		Assertions.assertEquals(false, us.deleteUser(0));
+		
+		
+	}
+	
+	@Test
+	void getUser() {
+		
+		int id = 1;
+		User userTest = new User();
+		userTest.setId(id);
+		userTest.setEmail("test@gmail.com");
+		userTest.setPassword("password1");
+		userTest.setFirstName("Jane");
+		userTest.setLastName("Doe");
+		
+	}
+	
+	@Test
+	void getUserByEmail() {
+		
+		String email = "test@gmail.com";
+		User user = new User();
+		User userTest = new User();
+		userTest.setId(1);
+		userTest.setEmail("test@gmail.com");
+		userTest.setPassword("password1");
+		userTest.setFirstName("Jane");
+		userTest.setLastName("Doe");
+		
+		Mockito.when(ur.findByEmail(email)).thenReturn(userTest);
+		
+		user=us.getUserByEmail(email);
+		Assertions.assertEquals("test@gmail.com", userTest.getEmail());
+		
+	}
+	
+	@Test
+	void getUserByEmailAndPassword() {
+		
+		String email = "test@gmail.com";
+		String password = "password1";
+		
+		User user = new User();
+		User userTest = new User();
+		userTest.setId(1);
+		userTest.setEmail("test@gmail.com");
+		userTest.setPassword("password1");
+		userTest.setFirstName("Jane");
+		userTest.setLastName("Doe");
+		
+		Mockito.when(ur.findByEmailAndPassword(email, password)).thenReturn(userTest);
+		
+		user = us.getUserByEmailAndPassword(email, password);
+		Assertions.assertEquals("test@gmail.com", userTest.getEmail());
+		Assertions.assertEquals("password1", userTest.getPassword());
+		
+	}
+	
+	@Test
+	void getAllUsers() {
+		
+		int userList = 3;
+		List<User> allUsers = new ArrayList<User>();
+		Mockito.when(ur.findAll()).thenReturn(Stream.of(new User(), new User(), new User()).collect(Collectors.toList()));
+		
+		allUsers = us.getAllUsers();
+		Assertions.assertEquals(userList, allUsers.size());
+	}
+	
 }
